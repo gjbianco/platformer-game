319a9e5ed9f220ab692c3da2aa03091451ac3bd5
Guy Bianco IV <gbianco@redhat.com>
2015-06-30 23:48:10 -0400

    #11 simple assets

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var mainState = {
 
   preload: function () {
+    game.load.image('player', 'assets/player.png');
   },
 
   create: function () {
+    game.stage.backgroundColor = '#3498db';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.player = game.add.sprite(250, 170, 'player');
   },
 
   update: function () {
